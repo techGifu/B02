4e727a2ead13be07d9cb352a88afcbe6e093a7df
D. FUKUOKA <30850626+techGifu@users.noreply.github.com>
2025-08-27 01:45:54 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,34 @@
-basic.forever(function () {
-	
-})
+namespace Board02 {
+    //% blockId=B02_SwitchON
+    //% block="スイッチをオンにする"
+    //% weight=80 blockGap=8
+    export function SwitchON(): void {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    }
+
+    //% blockId=B02_SwitchPWM
+    //% block="スイッチをオンにする（%Value ％）"
+    //% weight=80 blockGap=8 advanced=true
+    //% Value.min=0 Value.max=100 Value.defl=100
+    export function SwitchPWM(Value: number): void {
+        let val = (Value * 1023) / 100;
+        pins.analogWritePin(AnalogPin.P1, val)
+    }
+
+    //% blockId=B02_SwitchOFF
+    //% block="スイッチをオフにする"
+    //% weight=80 blockGap=8e
+    export function SwitchOFF(): void {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+
+    //% blockId=B02_Sensor
+    //% block="人感センサが反応した"
+    //% weight=80 blockGap=8
+    export function IsDetected(): boolean {
+        if (pins.digitalReadPin(DigitalPin.P2) == 1)
+            return true;
+        return false;
+    }
+
+}
